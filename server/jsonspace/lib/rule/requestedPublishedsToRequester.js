e35a4cab2af57d4e6f18277d0335c21b8202e577
chrisdew <chris.dew@barricane.com>
2016-01-16 07:18:17 +0000

    #8 comparison bug fix

--- server/jsonspace/lib/rule/requestedPublishedsToRequester.js
+++ server/jsonspace/lib/rule/requestedPublishedsToRequester.js
@@ -7,7 +7,7 @@ function exec(ob, put, queries) {
 
   const results = queries.published$channel.results(ob.requested_publisheds.channel);
   for (const result of results) {
-    if (result.id <= ob.requested_publisheds.published_since) continue;
+    if (result.id.substr(0, 24) <= ob.requested_publisheds.published_since) continue;
     const response = u.klone(result); // always klone before mutating
     delete response.published.conn_id;
     put({websocket_obj_tx: {conn_id: ob.requested_publisheds.conn_id, obj: response}});
