e3ea34f3f5321539f423b05de4a4b527db46723b
chrisdew <chris.dew@barricane.com>
2016-02-14 06:54:38 +0000

    closes #20

--- pubsub/lib/rule/watch.js
+++ pubsub/lib/rule/watch.js
@@ -32,13 +32,13 @@ function exec(ob, put, queries) {
   put(ob_to_put);
 
   // we no longer tell the originating client that they have subscribed, instead we just send a subscribers list
-  const response = {subscribers:{channel:ob.websocket_obj_rx.data.watch.channel,list:[]}};
-
-  const results = queries.subscribed$channel.results(ob.websocket_obj_rx.data.watch.channel);
-  for (const result of results) {
-    response.subscribers.list.push({username:result.subscribed.username,extra:result.subscribed.extra});
-  }
-  put({websocket_obj_tx:{conn_id:ob.websocket_obj_rx.conn_id,obj:response}});
+  put({
+    requested_subscribers: {
+      channel: ob.websocket_obj_rx.data.watch.channel,
+      conn_id: ob.websocket_obj_rx.conn_id,
+      username: ob.websocket_obj_rx.data.watch.username
+    }
+  });
 }
 
 exports.exec = exec;
