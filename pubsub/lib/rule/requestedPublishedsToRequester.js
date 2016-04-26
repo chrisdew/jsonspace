c932acbc4ffcb7ca2e6a393ec229e30cc0dce54c
chrisdew <chris.dew@barricane.com>
2016-04-26 05:56:11 +0100

    closes #53

--- pubsub/lib/rule/requestedPublishedsToRequester.js
+++ pubsub/lib/rule/requestedPublishedsToRequester.js
@@ -8,9 +8,14 @@ function exec(ob, put, queries) {
   const results = queries.published$channel.results(ob.requested_publisheds.channel);
   for (const result of results) {
     if (result.id.substr(0, 24) <= ob.requested_publisheds.published_since) continue;
-    const response = u.klone(result); // always klone before mutating
-    delete response.published.conn_id;
-    put({websocket_obj_tx: {conn_id: ob.requested_publisheds.conn_id, obj: response}});
+
+    // *never* mutate existing message objects, always klone first
+    const redacted = u.klone(result);
+    delete redacted.published.conn_id; // don't leak connection data
+    delete redacted.published.apn; // don't leak apple push data
+    delete redacted.published.gcm; // don't leak google push data
+
+    put({websocket_obj_tx: {conn_id: ob.requested_publisheds.conn_id, obj: redacted}});
   }
 
 }
